4830b64f83941d45a0158b27b3f000ec218c7491
MdFarzan <mdfarzan776@gmail.com>
2022-09-17 18:25:43 +0530

    db config added

---
+++ admin/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+    db.php
+    contains database related configuration
+*/
+
+define('DB_NAME', 'admin_panel');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
