a1dc5efdac222f82d9c6082d09fab5461f995bf1
MdFarzan <mdfarzan776@gmail.com>
2022-09-22 11:49:18 +0530

    base_url and description added for constants

--- app.php
+++ app.php
@@ -5,5 +5,9 @@
     contains basic info of application
 */
 
+// to show in admin panel and where necessary like password reset email etc.
 define('APP_NAME', 'Admin PLTE');
+
+// to refer links which is not part of admin panel
+define('BASE_URL', 'http://localhost/admin_panel/'); 
 
