b9c3428d68664b10386c404f04491f0fe461ab47
MdFarzan <mdfarzan776@gmail.com>
2022-09-22 12:07:06 +0530

    config file added for phpMailer and password-reset finish

---
+++ admin/config/phpmailer.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+    phpmailer.php
+    contains php mailer configuration
+*/
+
+
+define("MAILER_EMAIL", '');     // email to use for sending mail
+define("MAILER_PASSKEY", '');   // password of above email
