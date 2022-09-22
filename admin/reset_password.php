e5746cb849ea13b648e8f6767e1382e7f4af9d0e
MdFarzan <mdfarzan776@gmail.com>
2022-09-22 12:04:02 +0530

    otp and new-password submission added

--- admin/reset_password.php
+++ admin/reset_password.php
@@ -1,10 +1,83 @@
 <?php
 /* 
-        sign_in.php
-        sign_in form / entrypoint for admin
+        reset_password.php
+        contains final step for reset password
     */
 
 require_once '../app.php';
+require_once 'config/db.php';
+
+
+$OTPError = $PasswordError = '';
+
+session_start();
+if(!isset($_SESSION['PASS_REC_EXP'])){
+    header("Location: index.php");
+}
+
+else{
+    if($_SESSION['PASS_REC_EXP'] > time()){
+        $otp_expired = false;
+
+        $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+            if ($db->connect_error)
+                die("unable to connect to the database");
+
+        
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_SESSION ? : session_start();
+            $OTP = trim($_POST['otp']);
+            $new_password = password_hash($_POST['confirm-password'], PASSWORD_DEFAULT);
+            $user_id = $_SESSION['PASS_REC_UID'];
+
+            // checking is otp expired
+            if($_SESSION['PASS_REC_EXP'] > time()){
+                if($_SESSION['PASS_REC_OTP'] == $OTP){
+
+                    if(strlen($_POST['confirm-password']) > 6){
+                        
+                        // updating when everything is ok
+                        $sql = "UPDATE admin_auth SET password = '$new_password' WHERE id = '$user_id'";
+                        if($db->query($sql)){
+                            session_destroy();
+                            echo "<script>
+                                    alert('Password reset successfully!');
+                                    window.location.href = 'index.php';
+                                </script>";
+                        }
+
+                        else{
+                            die("Unable to reset password!");
+                        }
+                    
+
+                    }
+
+                    else{
+                        $PasswordError = 'Enter greater than 6 character password';
+                    }
+
+                }
+
+                else{
+                    $OTPError = 'Invalid OTP!';
+                }
+            }
+
+            else{
+                $otp_expired = true;
+
+            }
+         
+        }
+    }
+
+    else{
+        $otp_expired = true;
+        
+    }
+}
 
 ?>
 
@@ -29,30 +102,43 @@ require_once '../app.php';
 
         <div id="form-wrap" class="bg-white text-center">
             <h2 class="position-relative">Password Reset</h2>
-
-            <form class="form" action="">
-
-                <div class="input-group mb-4 mr-sm-2">
+            <?php if(!$otp_expired){ ?>
+            <form class="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                
+                <div class="mb-4">
+                <div class="input-group mr-sm-2">
                     <div class="input-group-prepend">
                         <div class="input-group-text"><i class="fa-solid fa-key"></i></div>
                     </div>
-                    <input type="password" name="password" class="form-control" placeholder="Password">
+                    <input type="text" name="otp" class="form-control" value="<?php echo isset($_POST['otp'])?$_POST['otp']:''; ?>" style="background:#fff;" placeholder="OTP" autofocus readonly
+                        onclick="this.removeAttribute('readonly')" required>
+                </div>
+                <p class="text-danger"><?php echo $OTPError; ?></p>
                 </div>
 
-                <div class="input-group mb-4 mr-sm-2">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa-solid fa-key"></i></div>
+                <div class="mb-4">
+                    <div class="input-group mr-sm-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fa-solid fa-key"></i></div>
+                        </div>
+                        <input type="password" name="confirm-password" value="<?php echo isset($_POST['confirm-password'])?$_POST['confirm-password']:''; ?>" class="form-control" placeholder="Confirm Password"
+                            onfocus="this.type = 'text';" onblur="this.type = 'password'" required>
                     </div>
-                    <input type="password" name="confirm-password" class="form-control" placeholder="Confirm Password">
+                    <p class="text-danger"><?php echo $PasswordError; ?></p>
                 </div>
 
                 <div class="text-center">
                     <button type="submit" class="btn btn-primary mb-2">Reset my Password</button>
                 </div>
             </form>
-
+            <?php }
+                else{
+                    echo '<div class="text-center text-danger"><h4>Your Password reset otp is expired!</h4></div>
+                            <p class="lead">To generate new one <a href="forget_password.php" title="Forget password">visit here</a></p>';
+                }
+            ?>
             <div class="text-center back">
-                <a href="#" class="text-secondary"><i class="fa-solid fa-arrow-left-long"></i>Back to site</a>
+                <a href="<?php echo BASE_URL; ?>" class="text-secondary"><i class="fa-solid fa-arrow-left-long"></i>Back to site</a>
             </div>
         </div>
     </div>
